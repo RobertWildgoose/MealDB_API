35599b5d242292dd8cb36e7ab51b2d5435d1ebec
Robert Wildgoose <contact@robertwildgoose.co.uk>
2023-12-02 00:34:19 +0000

    Initial upload of project
    To confirm that github actions are set up correctly.

---
+++ TheMealDB/Services/BaseService.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMealDB.Services
+{
+	public class BaseService
+	{
+		public readonly string BaseUrl = "www.themealdb.com/api/json/v1/1/";
+		public async Task<T> GetItem<T>(string url) where T : class
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				try
+				{
+					var fullUrl = $"{BaseUrl}{url}";
+					HttpResponseMessage response = await client.GetAsync(fullUrl);
+					response.EnsureSuccessStatusCode();
+					string json = await response.Content.ReadAsStringAsync();
+					return JsonConvert.DeserializeObject<T>(json ?? string.Empty);
+				}
+				catch (HttpRequestException ex)
+				{
+					Console.WriteLine($"Error: {ex.Message}");
+					return null;
+				}
+			}
+		}
+
+		public async Task<List<T>> GetList<T>(string url) where T : class
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				try
+				{
+					var fullUrl = $"{BaseUrl}{url}";
+					HttpResponseMessage response = await client.GetAsync(fullUrl);
+					response.EnsureSuccessStatusCode();
+					string json = await response.Content.ReadAsStringAsync();
+					return JsonConvert.DeserializeObject<List<T>>(json ?? string.Empty);
+				}
+				catch (HttpRequestException ex)
+				{
+					Console.WriteLine($"Error: {ex.Message}");
+					return null;
+				}
+			}
+		}
+	}
+}
---
+++ TheMealDB/Services/SearchService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheMealDB.Interfaces;
+
+namespace TheMealDB.Services
+{
+	public class SearchService : BaseService , ISearchService
+	{
+
+	}
+}
